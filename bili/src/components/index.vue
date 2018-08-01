6aa1ffa6e7156520ab80c03c92f97167aaa031e6
liuyang <442795048@qq.com>
2018-08-01 08:14:17 +0800

    liu-3

---
+++ bili/src/components/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+	  <navbar></navbar>
+	  <sidebar></sidebar>
+
+	  
+	</div>
+
+
+</template>
+
+<script type="text/javascript">
+	import navbar from "./common/navbar"
+	import sidebar from "./common/sidebar"
+	export default{
+		name:"index",
+		data(){
+			return {
+
+			}
+		},
+		components:{
+			navbar,
+			sidebar
+		}
+
+	}
+	
+</script>
+
+<style scoped lang="scss">
+	
+</style>
